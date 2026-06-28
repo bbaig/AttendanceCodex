a8429ad398df29c763fc08e7a3ee8e1039122521
bbaig <moghul.baig@gmail.com>
2026-06-28 18:48:00 +0530

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://nwdzkfssqprsoifnpesh.supabase.co";
-const SUPABASE_ANON_KEY = "YOUR_ANON_PUBLIC_KEY";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im53ZHprZnNzcXByc29pZm5wZXNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIwMTc1NzcsImV4cCI6MjA5NzU5MzU3N30.L1y9frTQASvQC0vLcRvaggpZxZl8zMfGvRSzYAzcohE";
 
 console.log("window.supabase:", window.supabase);
 
