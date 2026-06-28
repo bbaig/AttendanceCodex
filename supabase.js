067e85b2dfc92e74f5efab320dcbb1ec069b6ed5
bbaig <moghul.baig@gmail.com>
2026-06-28 18:46:13 +0530

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,7 +1,12 @@
 const SUPABASE_URL = "https://nwdzkfssqprsoifnpesh.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im53ZHprZnNzcXByc29pZm5wZXNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIwMTc1NzcsImV4cCI6MjA5NzU5MzU3N30.L1y9frTQASvQC0vLcRvaggpZxZl8zMfGvRSzYAzcohE";
+const SUPABASE_ANON_KEY = "YOUR_ANON_PUBLIC_KEY";
+
+console.log("window.supabase:", window.supabase);
 
 const supabase = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
+
+console.log("supabase client:", supabase);
+console.log("supabase.from:", supabase.from);
